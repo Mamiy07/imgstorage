b8e6e4bd2f156d771565de20ddbcb2cce0e573d1
mamiy07 <mamiy7469@gmail.com>
2026-04-10 11:18:36 +0000

    fixed prisma error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,13 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client/edge";
+import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    adapter,
+    log: ["query"],
   });
 
 if (process.env.NODE_ENV !== "production") {
